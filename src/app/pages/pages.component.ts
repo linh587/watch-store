71668560964f775c9a908a30725c9ed7a8331725
Linh <lemailinh22@gmail.com>
2024-02-02 01:45:05 +0700

    scroll to top

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,11 +1,36 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  HostListener,
+  Inject,
+  OnInit,
+  PLATFORM_ID,
+} from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
 
 @Component({
   selector: "app-pages",
   templateUrl: "./pages.component.html",
+  styleUrl: "./pages.component.scss",
 })
 export class PagesComponent implements OnInit {
-  constructor() {}
+  public showBackToTop: boolean = false;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {}
+
+  public scrollToTop() {
+    if (isPlatformBrowser(this.platformId)) window.scrollTo(0, 0);
+  }
+
+  @HostListener("window:scroll") onWindowScroll() {
+    const scrollTop = Math.max(
+      window.pageYOffset,
+      document.documentElement.scrollTop,
+      document.body.scrollTop
+    );
+    scrollTop > 300
+      ? (this.showBackToTop = true)
+      : (this.showBackToTop = false);
+  }
 }
