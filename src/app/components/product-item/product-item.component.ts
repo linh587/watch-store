5400dd056a8840ed2b1b4f96f64d6166c9c9cbf2
Linh <lemailinh22@gmail.com>
2024-03-06 21:42:29 +0700

    quick-view-product #1

--- src/app/components/product-item/product-item.component.ts
+++ src/app/components/product-item/product-item.component.ts
@@ -18,6 +18,7 @@ export class ProductItemComponent implements OnInit {
     event.preventDefault();
     const modalRef = this.modalService.open(QuickViewProductModalComponent, {
       centered: true,
+      size: "lg"
     });
 
     modalRef.componentInstance.productItem = this.productItem;
--- src/app/components/quick-view-product-modal/quick-view-product-modal.component.ts
+++ src/app/components/quick-view-product-modal/quick-view-product-modal.component.ts
@@ -1,19 +1,31 @@
-import { Component, Input, OnInit } from "@angular/core";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from "@angular/core";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import SwiperCore, { FreeMode, Navigation, Thumbs } from "swiper";
+
+SwiperCore.use([FreeMode, Navigation, Thumbs]);
 
 @Component({
   selector: "app-quick-view-product-modal",
   templateUrl: "./quick-view-product-modal.component.html",
   styleUrls: ["./quick-view-product-modal.component.scss"],
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class QuickViewProductModalComponent implements OnInit {
-  @Input() productItem: any;
-  public initialSlide: number = 0;
+  public productItem: any;
   public thumbsSwiper: any;
-  public slidesPerView: number = 10;
+  public slidesPerView: number = 4;
 
-  constructor() {}
+  constructor(private modalService: NgbModal) {}
 
   ngOnInit() {}
 
-  public activeIndexChange(event: any) {}
+  public onCloseModal() {
+    this.modalService.dismissAll();
+  }
 }
